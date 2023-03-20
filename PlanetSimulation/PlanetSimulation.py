f84e27a5682253d77adbc1e37ddf083e5c7ce496
Adel <adi.stein@outlook.cz>
2023-03-20 19:59:16 +0100

    text, menu, start button

--- PlanetSimulation/PlanetSimulation.py
+++ PlanetSimulation/PlanetSimulation.py
@@ -1,9 +1,12 @@
 import pygame
 import math
+import button
+import sys
+
 
 pygame.init()
 
-WIDTH, HEIGHT = 800,800
+WIDTH, HEIGHT = 1200,800
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
@@ -14,15 +17,27 @@ RED = (188,39,50)
 DARK_GREY = (80,78,81)
 BLACK = (0,0,0)
 GREEN = (80, 200, 120)
+TEXT_COL = (255, 0, 255)
+font = pygame.font.SysFont(None, 24)
+
+start_img = pygame.image.load('start.png').convert_alpha()
+exit_img = pygame.image.load('menu.png').convert_alpha()
 
 
 
+start_button = button.Button(300, 300, start_img, 3)
+exit_button = button.Button(450, 200, exit_img, 3)
+
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  WIN.blit(img, (x, y))
 
 class Planet:
     AU = 149.6e6 * 1000
     G = 6.67428e-11
     SCALE = 200 / AU #jeden AU se rovná 100 pixelů
     TIMESTEP = 3600*24 #jeden den
+    
 
 
     def __init__(self, x, y, radius, color, mass):
@@ -58,6 +73,7 @@ class Planet:
         pygame.draw.circle(win, self.color, (x, y), self.radius )
 
     def attraction(self, other):
+
         other_x, other_y = other.x, other.y
         distance_x = other_x - self.x
         distance_y = other_y - self.y
@@ -70,10 +86,22 @@ class Planet:
         theta = math.atan2(distance_y, distance_x)
         force_x = math.cos(theta) * force 
         force_y = math.sin(theta) * force
+
+
+        with open("data.txt", "a") as f:
+            data = distance, force, theta 
+            f.write( "\n")
+            f.write("Delka, sila, uhel\n")
+            f.write(str(data))
+            f.write("\n")
+
+
         return force_x, force_y
+
     
 
     def update_position(self, planets):
+
         total_fx = total_fy = 0
         for planet in planets:
             if self == planet:
@@ -96,7 +124,7 @@ def main():
     run  = True
     clock = pygame.time.Clock()
 
-    sun = Planet(0,0,30, YELLOW, 1.98892 * 10**30 )
+    sun = Planet(0,0,30, YELLOW, 1.989 * 10**30 )
     sun.sun = True
 
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24) 
@@ -105,8 +133,6 @@ def main():
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
     mars.y_vel = 24.077 * 1000
 
-    AS = Planet(0.55 * Planet.AU, 0, 9, GREEN, 2.65 * 10**23)
-    AS.y_vel = -35.08 * 1000
 
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
     mercury.y_vel = -47.4 * 1000
@@ -115,7 +141,9 @@ def main():
     venus.y_vel = 35.02 * 1000
 
 
-    planets = [sun,earth,mars, AS, mercury, venus]
+    planets = [sun,earth,mars, mercury, venus]
+
+
 
 
 
@@ -126,7 +154,8 @@ def main():
         WIN.fill((0,0,0))
         #pygame.display.update()
 
-
+        draw_text("HALOO HALOO", font, TEXT_COL,160,300)
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False 
@@ -134,6 +163,7 @@ def main():
             planet.update_position(planets)
             planet.draw(WIN)
             
+
         pygame.display.update() 
     
     pygame.quit()
